2a02f93989e46272c52f45be02bfb527eb3ae892
SeungHun333 <yui332@naver.com>
2025-05-07 14:57:47 +0900

    User Entity 설계

---
+++ src/main/java/com/reallyeasy/cineView/domain/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.reallyeasy.cineView.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Table(name = "users")
+@Getter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String userName;
+    
+    private String password;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 100)
+    private String bio;
+
+    private Character gender;
+}
