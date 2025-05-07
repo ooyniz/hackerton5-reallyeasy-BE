5a1275480e30ec2617f8a81c96f3181cef2ccf26
SeungHun333 <yui332@naver.com>
2025-05-07 13:49:37 +0900

    프로젝트 생성

---
+++ src/test/java/com/reallyeasy/cineView/CineViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reallyeasy.cineView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
