c2280eb0d22176baf7f822d5551b6dd6bb42cb9f
ooyniz <ooyniz@gmail.com>
2025-05-07 16:34:46 +0900

    feat: baseTimeEntity 추가

---
+++ src/main/java/com/reallyeasy/cineView/domain/BaseTimeEntity.java
@@ -0,0 +1,31 @@
+package com.reallyeasy.cineView.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    public void markDeleted() {
+        this.deletedAt = LocalDateTime.now();
+    }
+}
